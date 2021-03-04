99a9148c09cea3346a9009b42b01586cd3fa0a2d
Mikhail <strwolfy@gmail.com>
2021-03-04 12:51:42 +0300

    Initial commit

---
+++ src/main/java/web/dao/UserDao.java
@@ -0,0 +1,30 @@
+package web.dao;
+
+import web.model.Role;
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    List<User> getAllUsers();
+
+    void saveUser(User user);
+
+    User getUser(int id);
+
+    void editUser(User user);
+
+    void deleteUser(int id);
+
+    User showUserByUsername(String email);
+
+
+
+//перенес логику в RoleDao
+
+/*    Role getRoleByName(String name);
+
+    List<Role> getListRole();*/
+
+}
+
